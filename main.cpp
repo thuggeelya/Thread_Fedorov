ddfa3c81a5d6fe042ba1195feeabaa600bbe53e7
thuggeelya <ilya-fdmdh@mail.ru>
2022-09-08 01:18:46 +0700

    Statistics added

--- main.cpp
+++ main.cpp
@@ -5,52 +5,71 @@
 #include <numeric>
 
 int main() {
+    int nIterations = 10;
+    std::vector<long long> timeMultipleThread(nIterations);
+    std::vector<long long> timeSingleThread(nIterations);
     unsigned int nThreads = std::thread::hardware_concurrency();
     long long size = 10000000000LL;
-    std::vector<unsigned long long> sumVector(nThreads, 0);
     long long step = size / nThreads;
-    auto start = std::chrono::system_clock::now();
 
-    auto separateSum{
-            [&sumVector](unsigned int pos, unsigned long long first, unsigned long long last) {
-                unsigned long long sum = 0;
+    for (int i = 0; i < nIterations; ++i) {
+        std::cout << "\nIteration #" + std::to_string(i) << std::endl;
+        std::vector<unsigned long long> sumVector(nThreads, 0);
+        auto start = std::chrono::system_clock::now();
 
-                for (unsigned long long i = first; i < last; ++i) {
-                    sum += i;
+        auto separateSum{
+                [&sumVector](unsigned int pos, unsigned long long first, unsigned long long last) {
+                    unsigned long long sum = 0;
+
+                    for (unsigned long long i = first; i < last; ++i) {
+                        sum += i;
+                    }
+
+                    sumVector.at(pos) = sum;
                 }
+        };
 
-                sumVector.at(pos) = sum;
-            }
-    };
+        std::vector<std::thread> threadVector;
 
-    std::vector<std::thread> threadVector;
+        for (unsigned int k = 0; k < nThreads; ++k) {
+            long long remains = (k == nThreads - 1)
+                                ? size % nThreads
+                                : 0;
+            threadVector.emplace_back(separateSum, k, k * step, (k + 1) * step + remains);
+        }
 
-    for (unsigned int i = 0; i < nThreads; ++i) {
-        long long remains = (i == nThreads - 1)
-                            ? size % nThreads
-                            : 0;
-        threadVector.emplace_back(separateSum, i, i * step, (i + 1) * step + remains);
+        for (auto &thread: threadVector) {
+            thread.join();
+        }
+
+        unsigned long long result = std::accumulate(sumVector.begin(), sumVector.end(), (unsigned long long) 0LL);
+        auto end = std::chrono::system_clock::now();
+        auto elapsed_nanoseconds = (end - start).count();
+        std::cout << std::to_string(nThreads) + " threads:\n" + std::to_string(result) + ". Time: " +
+                     std::to_string(elapsed_nanoseconds) + " nanoseconds" << std::endl;
+        timeMultipleThread.at(i) = elapsed_nanoseconds;
+
+        start = std::chrono::system_clock::now();
+        unsigned long long res1 = 0;
+
+        for (unsigned long long j = 1; j <= size; ++j) {
+            res1 += j;
+        }
+
+        end = std::chrono::system_clock::now();
+        elapsed_nanoseconds = (end - start).count();
+        std::cout << "One thread:\n" + std::to_string(res1) + ". Time: " + std::to_string(elapsed_nanoseconds) +
+                     " nanoseconds" << std::endl;
+        timeSingleThread.at(i) = elapsed_nanoseconds;
     }
 
-    for (auto &thread: threadVector) {
-        thread.join();
-    }
-
-    unsigned long long result = std::accumulate(sumVector.begin(), sumVector.end(), (unsigned long long int) 0LL);
-    auto end = std::chrono::system_clock::now();
-    auto elapsed_seconds = end - start;
-    std::cout << std::to_string(nThreads) + " threads:\n" + std::to_string(result) + ". Time: " +
-                 std::to_string(elapsed_seconds.count()) + " seconds" << std::endl;
     std::cout << std::endl;
-    start = std::chrono::system_clock::now();
-    unsigned long long res1 = 0;
-
-    for (unsigned long long i = 1; i <= size; ++i) {
-        res1 += i;
-    }
-
-    end = std::chrono::system_clock::now();
-    elapsed_seconds = end - start;
-    std::cout << "One thread:\n" + std::to_string(res1) + ". Time: " + std::to_string(elapsed_seconds.count()) +
-                 " seconds";
+    long long avgMultiple = std::accumulate(timeMultipleThread.begin(), timeMultipleThread.end(),
+                                            (long long) 0LL) / nIterations;
+    long long avgSingle = std::accumulate(timeSingleThread.begin(), timeSingleThread.end(),
+                                          (long long) 0LL) / nIterations;
+    std::cout << "Average time for multiple threads (nanoseconds): " + std::to_string(avgMultiple) << std::endl;
+    std::cout << "Average time for single thread (nanoseconds)   : " + std::to_string(avgSingle) << std::endl;
+    std::cout << "Multithreading is faster in " + std::to_string((double) avgSingle / (double) avgMultiple) + " times"
+              << std::endl;
 }
